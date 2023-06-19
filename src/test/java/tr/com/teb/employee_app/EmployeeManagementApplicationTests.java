fb7604326eec5473ff67745c524b70d9cc024448
Kemal bayram <kemalbayramag@gmail.com>
2023-06-19 11:07:47 +0300

    Spring project initialize

---
+++ src/test/java/tr/com/teb/employee_app/EmployeeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package tr.com.teb.employee_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
