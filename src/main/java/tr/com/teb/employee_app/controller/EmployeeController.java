3adeae6bdd03768b154cca4d96d3bbd5c933654c
Kemal bayram <kemalbayramag@gmail.com>
2023-06-20 13:53:34 +0300

    ekrana aktarılacak olan Model nesnesine employeeList key değeri ile employeeList verildi ve ekrana aktarıldı.

--- src/main/java/tr/com/teb/employee_app/controller/EmployeeController.java
+++ src/main/java/tr/com/teb/employee_app/controller/EmployeeController.java
@@ -3,13 +3,21 @@ package tr.com.teb.employee_app.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import tr.com.teb.employee_app.modal.entity.Employee;
 import tr.com.teb.employee_app.modal.service.AbstractEmployeeService;
+
+import java.util.List;
 
 @Controller
 public class EmployeeController {
     @Autowired
     private AbstractEmployeeService employeeService;
+
+    @RequestMapping("/")
     public String startIndexPage(Model model){
+        List<Employee> employeeList = employeeService.getAll();
+        model.addAttribute("employeeList", employeeList);
         return "index";
     }
 
