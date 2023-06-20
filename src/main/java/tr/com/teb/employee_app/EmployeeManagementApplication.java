aeafdd0bdacccd56087fdc74f7d0817b9d2894d1
Kemal bayram <kemalbayramag@gmail.com>
2023-06-20 16:18:02 +0300

    uygulama url ile çağrıldığında login sayfasına yönlendirmesi engellendi.

--- src/main/java/tr/com/teb/employee_app/EmployeeManagementApplication.java
+++ src/main/java/tr/com/teb/employee_app/EmployeeManagementApplication.java
@@ -2,9 +2,10 @@ package tr.com.teb.employee_app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import tr.com.teb.employee_app.modal.entity.Employee;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class EmployeeManagementApplication {
 
 	public static void main(String[] args) {
--- src/main/java/tr/com/teb/employee_app/controller/EmployeeController.java
+++ src/main/java/tr/com/teb/employee_app/controller/EmployeeController.java
@@ -1,15 +1,11 @@
 package tr.com.teb.employee_app.controller;
 
-import javafx.stage.Modality;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import tr.com.teb.employee_app.modal.entity.Employee;
 import tr.com.teb.employee_app.modal.service.AbstractEmployeeService;
-
-import java.time.LocalDate;
-import java.util.Date;
 import java.util.List;
 
 @Controller
